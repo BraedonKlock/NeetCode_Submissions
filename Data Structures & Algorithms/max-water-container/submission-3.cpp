33bf82a7fb5d694c4d03757bb34a6d6d78e65788
Braedon Klock <braedonk@hotmail.com>
2026-07-24 06:14:49 -0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        size_t result {0};
+
+        size_t i {0};
+        size_t r {heights.size() - 1};
+
+        while (i < r) {
+            size_t area {min(heights[i], heights[r]) * (r - i)};
+            result = max(result, area);
+
+            if (heights[i] <= heights[r]) i++;
+            else r--;
+        }
+        return result;
+    }
+};
